d69c084cbf279d978c71d70d0feae307725fc657
AdamSeyda <77727546+AdamSeyda@users.noreply.github.com>
2022-02-27 19:04:35 +0100

    Delete Guardian of the Gate - code directory

--- Guardian of the Gate - code/lvlup.h
+++
@@ -1,9 +0,0 @@
-#ifndef lvlup
-#define lvlup
-#include "herostatistics.h"
-
-void levelup(herostats*);
-// function called after a battle if hero.experience reaches certain numbers. It controls the process of leveling up
-
-#endif
-
